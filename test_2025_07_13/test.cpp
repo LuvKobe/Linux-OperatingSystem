f467a3f854ef1a3cdb006285857449db8d027054
edison <3078601436@qq.com>
2025-07-13 23:37:08 +0800

    study code

---
+++ test_2025_07_13/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <unistd.h>
+#include <cassert>
+#include <sys/wait.h>
+#include <sys/types.h>
+using namespace std;
+
+int main()
+{
+    ;
+    return 0;
+}
